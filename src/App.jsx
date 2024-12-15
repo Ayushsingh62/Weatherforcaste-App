c75980f6d5c8910f8b59176904817506cdd496a2
Ayushsingh62 <ayushsinghparihar48@gmail.com>
2024-12-15 23:06:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Weather_list from './components/Weather_list'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="container">
+
+
+    <h1>Weather Forecast</h1>
+
+    </div>
+
+    
+<Weather_list/>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Box.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Box() {
+
+
+  return (
+    
+
+    <div className="box">
+
+    <input type="text" name="" id="name"  />
+    
+    <button>submit</button>
+    
+        </div>
+  )
+}
+
+export default Box
